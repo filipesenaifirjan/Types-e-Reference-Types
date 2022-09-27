783a15d55e7fa348b5337d05a20e5ec4aff39f86
filipesenaifirjan <filipemattossenai@gmail.com>
2022-09-27 08:18:39 -0300

    Types

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console; //Assim não precisa escrever Console. antes de imprimir o que foi lido acima
+public class Program{
+
+    public static void main()
+    {
+        WriteLine("Hello Word");
+    }
+}
+
